91ad2ff6208526858203f684abcf7e18fcbf6269
Ed Schouten <ed@kumina.nl>
2017-10-19 16:23:50 +0200

    Add a script for building a statically linked executable.
    This is the script we use at Kumina to build binaries that can be
    deployed across our cluster easily.

---
+++ build_static.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker run -i -v `pwd`:/unbound_exporter alpine:edge /bin/sh << 'EOF'
+set -ex
+
+# Install prerequisites for the build process.
+apk update
+apk add ca-certificates git go libc-dev
+update-ca-certificates
+
+# Build the unbound_exporter.
+cd /unbound_exporter
+export GOPATH=/gopath
+go get -d ./...
+go build --ldflags '-extldflags "-static"'
+strip unbound_exporter
+EOF
