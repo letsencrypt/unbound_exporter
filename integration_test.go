52740496955c9fadd242d2331a99ac43f63f584c
Matthew McPherrin <git@mcpherrin.ca>
2023-11-16 22:54:08 -0500

    Fill out a simple integration test
    This is a simple smoketest to ensure the unbound_exporter is
    successfully serving metrics, and that it thinks it can scrape from
    Unbound.

--- integration_test.go
+++ integration_test.go
@@ -2,7 +2,12 @@
 
 package main
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+
+	"github.com/prometheus/common/expfmt"
+)
 
 // TestIntegration checks that unbound_exporter is running, successfully
 // scraping and exporting metrics.
@@ -16,5 +21,32 @@ import "testing"
 //	go test --tags=integration
 //	docker compose down
 func TestIntegration(t *testing.T) {
-	// TODO
+	resp, err := http.Get("http://localhost:9167/metrics")
+	if err != nil {
+		t.Fatalf("Failed to fetch metrics from unbound_exporter: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected a 200 OK from unbound_exporter, got: %v", resp.StatusCode)
+	}
+
+	parser := expfmt.TextParser{}
+	metrics, err := parser.TextToMetricFamilies(resp.Body)
+	if err != nil {
+		t.Fatalf("Failed to parse metrics from unbound_exporter: %v", err)
+	}
+
+	// unbound_up is 1 if we've successfully scraped metrics from it
+	unbound_up := metrics["unbound_up"].Metric[0].Gauge.GetValue()
+	if unbound_up != 1 {
+		t.Errorf("Expected unbound_up to be 1, not: %v", unbound_up)
+	}
+
+	// Check some expected metrics are present
+	for _, metric := range []string{"go_info", "unbound_queries_total", "unbound_response_time_seconds", "unbound_cache_hits_total"} {
+		if _, ok := metrics[metric]; !ok {
+			t.Errorf("Expected metric is missing: %s", metric)
+		}
+	}
 }
