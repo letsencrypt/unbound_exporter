25989c3275cc3692622c10b6b4fa79c0d4a66d36
Matthew McPherrin <git@mcpherrin.ca>
2025-11-13 14:49:01 -0500

    Use a proper html template to render the homepage
    Adds a test, and makes the /_healthz link customizable and linked from the
    homepage too.
    
    While malicious command line flags doesn't make sense from a security point of
    view, it does prevent breaking the html if your desired paths have any special
    characters in them.
    
    This also pulls the server setup into its own function to keep things tidier.

--- unbound_exporter_test.go
+++ unbound_exporter_test.go
@@ -2,8 +2,21 @@ package main
 
 import "testing"
 
-func TestStub(t *testing.T) {
-	if 1 != 1 { //nolint
-		t.Fatal("Math is a lie. We should never have taught computers to think.")
+// TestTemplate ensures the template for the homepage of unbound_exporter renders and properly escapes
+func TestTemplate(t *testing.T) {
+	const expected = `
+<html>
+	<head><title>Unbound Exporter</title></head>
+	<body>
+		<h1>Unbound Exporter</h1>
+		<p><a href='/metrics%27%20bad=%27true%27'>Metrics</a></p>
+		<p><a href='/_healthz'>Health</a></p>
+	</body>
+</html>
+`
+
+	result := string(homePageText("/metrics' bad='true'", "/_healthz"))
+	if result != expected {
+		t.Fatalf("Unexpected result: '%s' is not '%s'", result, expected)
 	}
 }
