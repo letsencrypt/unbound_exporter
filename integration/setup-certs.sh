b2cd03f741b166e008486a5113353537d7c1d273
Matthew McPherrin <git@mcpherrin.ca>
2026-01-07 16:57:52 -0500

    simplify by just having three different configs

--- integration/setup-certs.sh
+++ integration/setup-certs.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
 set -e
 
-cd /certs
-
-if [ -f unbound_ca_ec.pem ]; then
-  echo 'Certificates already exist'
-  exit 0
-fi
-
 # Generate CA key and certificate
-openssl ecparam -genkey -name secp384r1 -out unbound_ca_ec.key
-openssl req -new -x509 -days 397 -key unbound_ca_ec.key -out unbound_ca_ec.pem \
+openssl ecparam -genkey -name secp384r1 -out unbound_ca.key
+
+openssl req -new -x509 -days 90 -key unbound_ca.key -out unbound_ca.pem \
   -subj "/CN=unbound-ca" \
-  -addext "subjectKeyIdentifier=hash" \
-  -addext "authorityKeyIdentifier=keyid:always,issuer:always" \
   -addext "basicConstraints=critical,CA:true"
 
 # Generate server key and certificate
-openssl ecparam -genkey -name secp384r1 -out unbound_server_ec.key
-openssl req -new -key unbound_server_ec.key -out server.csr -subj "/CN=unbound"
-openssl x509 -req -in server.csr -CA unbound_ca_ec.pem -CAkey unbound_ca_ec.key \
-  -CAcreateserial -out unbound_server_ec.pem -days 397 -sha256 \
-  -addext "subjectAltName=DNS:unbound,DNS:localhost" \
-  -addext "subjectKeyIdentifier=hash" \
-  -addext "authorityKeyIdentifier=keyid:always,issuer:always" \
-  -addext "extendedKeyUsage=serverAuth"
+openssl ecparam -genkey -name secp384r1 -out unbound_server.key
 
-# Append CA cert to server cert for chain
-cat unbound_ca_ec.pem >> unbound_server_ec.pem
+openssl req -new -key unbound_server.key -out server.csr -subj "/" \
+  -addext "subjectAltName = DNS:unbound,DNS:localhost" \
+  -addext "extendedKeyUsage = serverAuth"
+
+openssl x509 -req -in server.csr -CA unbound_ca.pem -CAkey unbound_ca.key \
+  -CAcreateserial -out unbound_server.pem -days 90 \
 
 # Generate client key and certificate
-openssl ecparam -genkey -name secp384r1 -out unbound_control_ec.key
-openssl req -new -key unbound_control_ec.key -out client.csr -subj "/CN=unbound-control"
-openssl x509 -req -in client.csr -CA unbound_ca_ec.pem -CAkey unbound_ca_ec.key \
-  -CAcreateserial -out unbound_control_ec.pem -days 397 -sha256 \
-  -addext "extendedKeyUsage=clientAuth"
+openssl ecparam -genkey -name secp384r1 -out unbound_control.key
+
+openssl req -new -key unbound_control.key -out client.csr -subj "/CN=unbound-control"
+
+openssl x509 -req -in client.csr -CA unbound_ca.pem -CAkey unbound_ca.key \
+  -CAcreateserial -out unbound_control.pem -days 90 \
 
 # Set permissions
 chmod 0640 *.key
